f5c0e5985cea67b132181f3ae9030606ea87fe5a
jhermann <jh@web.de>
2015-04-11 23:55:59 +0200

    :construction: added 'sync-labels' task

--- tasks.py
+++ tasks.py
@@ -1,3 +1,7 @@
+import os
+import tempfile
+
+import requests
 from invoke import ctask as task
 
 LABELS = ('Ready', 'In Progress')
@@ -10,6 +14,8 @@ PROJECTS = """
     Build-The-Web/bootils
 """
 PROJECTS = [i.strip() for i in PROJECTS.splitlines() if i]
+LABEL_MASTER_URL = 'https://raw.githubusercontent.com/jhermann/gh-commander/master/examples/labels.yaml'
+
 
 @task
 def markup(ctx):
@@ -35,3 +41,16 @@ def markup(ctx):
                 )
         lines.append(line)
     print('\n'.join(lines))
+
+
+@task(name='sync-labels')
+def sync_labels(ctx):
+    """Sync labels into managed projects."""
+    labels_yaml = requests.get(LABEL_MASTER_URL).text
+    with tempfile.NamedTemporaryFile(suffix='.yaml', prefix='gh-label-sync-', delete=False) as handle:
+        handle.write(labels_yaml)
+
+    try:
+        ctx.run('gh label import {} from {}'.format(' '.join(PROJECTS), handle.name))
+    finally:
+        os.remove(handle.name)
